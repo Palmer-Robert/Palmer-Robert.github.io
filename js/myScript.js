f321dff765d7859de23e38669a7de6c59c36f096
Palmer-Robert <robert.palmer1229@gmail.com>
2018-06-07 22:09:58 -0700

    Assignment submission
    I am submitting CSS manipulation and loops, conditional statements, etc.

--- js/myScript.js
+++ js/myScript.js
@@ -1,1 +1,65 @@
-function 
+// These are all functions for each of the buttons manipulatign the paragraphs.
+/********************************************
+Shopping list appendChild function
+********************************************/
+function shoppingList() {
+    var li = document.createElement("li");
+    var node = document.createTextNode(document.getElementById('addList').value);
+    li.appendChild(node);
+                
+    var element = document.getElementById('shop_list');
+    element.appendChild(li);
+}
+/*******************************************************
+Beginning of the buttons on the paragraphs
+*******************************************************/
+function insertPara() {
+    var para = document.createElement("p");
+    var node = document.createTextNode("Hi there! I am an insertBefore() method.");
+    para.appendChild(node);
+                
+    var element = document.getElementById("myDiv");
+    var child = document.getElementById("p1");
+    element.insertBefore(para, child);
+}
+
+function removePara() {
+    var myChild = document.getElementById("p3");
+    myChild.parentNode.removeChild(myChild); 
+}
+
+function replaceMe() {
+    var pg = document.createElement("p");
+    var node = document.createTextNode("Hello! I like to replace paragraphs. I just replaced paragraph 2.");
+    pg.appendChild(node);
+    
+    var parent = document.getElementById("myDiv");
+    var child = document.getElementById("p2");
+    parent.replaceChild(pg, child);
+}
+
+/***************************************************************
+
+***************************************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
