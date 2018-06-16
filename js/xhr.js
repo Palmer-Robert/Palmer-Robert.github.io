145fd8a81d12c019b3cc5d59f887d915f4995c31
Palmer-Robert <robert.palmer1229@gmail.com>
2018-06-16 10:24:12 -0700

    developing by Robert
    I am deveolping Local Storage and CSS

--- js/xhr.js
+++ js/xhr.js
@@ -1,8 +1,1 @@
-var family = new XMLHttpRequest();
-family.open('GET', 'https://raw.githubusercontent.com/Palmer-Robert/Palmer-Robert.github.io/master/palmerfam.json', true);
-family.onload = function() {
-    var myData = JSON.parse(family.responseText);
-    
-    console.log(myData[3].name)
-};
-family.send();
+var xhttp = new XMLHttpRequest();
